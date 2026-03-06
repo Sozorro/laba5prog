1cdd6fa4c9c4f0181ca912384ab29290d0857c66
Виктория Пегушина <kessi.kissa@MacBook-Air-von-Viktoria.local>
2026-03-06 22:48:28 +0300

    Add fields for commands

--- manager/ComParser.java
+++ manager/ComParser.java
@@ -7,18 +7,18 @@ import exeptions.WrongParam;
 
 public class ComParser {
     private HashMap<String, Command> commands = new HashMap<>();
-    ComParser() {
-        commands.put("AddCom", new AddCom());
-        commands.put("ClearCom", new ClearCom());
-        commands.put("ExecuteScriptCom", new ExecuteScriptCom());
-        commands.put("ExitCom", new ExitCom());
-        commands.put("HelpCom", new HelpCom());
-        commands.put("HistoryCom", new HistoryCom());
-        commands.put("InfoCom", new InfoCom());
-        commands.put("RemoveCom", new RemoveCom());
-        commands.put("SaveCom", new SaveCom());
-        commands.put("ShowCom", new ShowCom());
-        commands.put("UpdateCom", new UpdateCom());
+    public ComParser(CollectionManeger collectionManeger) {
+        commands.put("AddCom", new AddCom(collectionManeger));
+        commands.put("ClearCom", new ClearCom(collectionManeger));
+        commands.put("ExecuteScriptCom", new ExecuteScriptCom(collectionManeger));
+        commands.put("ExitCom", new ExitCom(collectionManeger));
+        commands.put("HelpCom", new HelpCom(collectionManeger));
+        commands.put("HistoryCom", new HistoryCom(collectionManeger));
+        commands.put("InfoCom", new InfoCom(collectionManeger));
+        commands.put("RemoveCom", new RemoveCom(collectionManeger));
+        commands.put("SaveCom", new SaveCom(collectionManeger));
+        commands.put("ShowCom", new ShowCom(collectionManeger));
+        commands.put("UpdateCom", new UpdateCom(collectionManeger));
 
     }
 

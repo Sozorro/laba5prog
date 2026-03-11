305f9a09b23cfea55d264aeac88653bcf78a7196
Виктория Пегушина <kessi.kissa@MacBook-Air-von-Viktoria.local>
2026-03-11 11:34:53 +0300

    realised read file

--- io/InputFile.java
+++ io/InputFile.java
@@ -1,27 +1,72 @@
 package io;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
-import manager.CollectionManager;
-import manager.ComParser;
+import managers.CollectionManager;
+import managers.ComParser;
 
 public class InputFile {
     private static ComParser comParser;
+    public static Scanner scannerNow;
+    public static boolean readFile = false;
+    private static String str = "";
+    private static String[] col;
+    private static String[] command;
+
     public static void start(File myFile, CollectionManager collectionManager) {
-        try {
+        try (InputStreamReader file = new InputStreamReader(new FileInputStream(myFile))) {
             comParser = new ComParser(collectionManager);
-            Scanner scanner = new Scanner(myFile);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
+            int c = file.read();
+            while (c != '\n') {
+                str += (char) c;
+                c = file.read();
             }
-            scanner.close();
+            col = str.split(",");
+            while (c != -1) {
+                if ((c == '\n' || c == '\r') && str != "") {
+                    command = str.split(",");
+                    comParser.interpret(command[0]);
+                    str = "";
+                }
+                else {
+                    str += (char) c;
+                }
+                c = file.read();
+            }
         } 
         catch (FileNotFoundException e) {
             //throw new WrongParam("Данный файл не найден");
+        } catch (IOException e) {       
+        } 
+    }
+    public static String getParams(String... s){
+        if(s[0] == "Id") {
+            return command[1];
         }
+        else if(s[0] == "Person") {
+            if(s[1] == "name") return command[10];
+            else if(s[1] == "height") return command[11];
+            else if(s[1] == "weight") return command[12];
+            else if(s[1] == "passportID") return command[13];
+            else if(s[1] == "hairColor") return command[14];
+        }
+        else if(s[0] == "LabWork") {
+            if(s[1] == "date") return command[2];
+            else if(s[1] == "name") return command[3];
+            else if(s[1] == "coordinatesX") return command[4];
+            else if(s[1] == "coordinatesY") return command[5];
+            else if(s[1] == "minimalPoint") return command[6];
+            else if(s[1] == "personalQualitiesMinimum") return command[7];
+            else if(s[1] == "description") return command[8];
+            else if(s[1] == "difficulty") return command[9];
+            else if(s[1] == "person") return getParams("Person", s[2]);
+        }
+        return null;
     }
     /*
     Должно быть:
